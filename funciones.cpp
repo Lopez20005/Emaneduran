a1acc7e55b6ebd5167631d140b037a77e68c5611
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-28 08:27:37 -0600

    prueba prof duran

---
+++ funciones.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<float> notas;
+    int Num_Materias;
+    
+    cout << "Ingrese la cantidad de materias cursadas en el semestre: ";
+    cin >> Num_Materias;
+    
+    for (int i = 0; i < Num_Materias; i++) {
+        float nota;
+        cout << "Ingrese la nota de la materia " << i+1 << ": ";
+        cin >> nota;
+    }
+    
+    cout << "Todas las notas almacenadas: ";
+    for (float nota : notas) {
+        cout << nota << " ";
+    }
+    float sum = 0;
+    for (float nota : notas) {
+        sum += nota;
+    }
+    float promedio = sum / Num_Materias;
+    cout << "\nPromedio de notas del semestre: " << promedio << endl;
+    
+    float nota_maxima = *max_element(notas.begin(), notas.end());
+    float nota_minima = *min_element(notas.begin(), notas.end());
+    cout << "Nota más alta obtenida: " << nota_maxima << endl;
+    cout << "Nota más baja obtenida: " << nota_minima << endl;
+
+    /*con mucho esfuerzo se logro, creo JAJAA, necesito ayuda por que para serle sincero vi un video para el promedio por que me costo*/
+    
+    return 0;
+}
+
+
+
+
+
---
+++ principal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+
+
+using namespace std ; 
+
+int main (){
+
+ int numMaterias;
+    int notas;
+    int nota;
+
+cout << "Ingrese la cantidad de materias: ";
+    cin >> numMaterias;
+
+ for (int i = 0; i < numMaterias; i++) {
+        cout << "Ingrese la nota de la materia " << i+1 << endl ; 
+        cin >> nota ; 
+        
+cout << "Notas almacenadas: ";
+cin >> notas ; 
+
+
+ }
+
+
+}
+ 
